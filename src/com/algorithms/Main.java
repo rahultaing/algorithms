a2c74f4f4bea18754b1d98c46135153650a95372
Rahul Taing <rtaing@IT-SEA17029-MBP.local>
2018-06-14 00:10:55 -0700

    word break

--- src/com/algorithms/Main.java
+++ src/com/algorithms/Main.java
@@ -1,7 +1,5 @@
 package com.algorithms;
 
-import com.algorithms.strings.LongestCommonPrefix;
-
 public class Main {
 
     public static void main(String[] args) {
---
+++ src/com/algorithms/dp/WordBreak.java
@@ -0,0 +1,27 @@
+package com.algorithms.dp;
+
+import java.util.HashSet;
+
+public class WordBreak {
+
+    public boolean wordBreak(String s, HashSet<String> wordDict) {
+
+        Integer n = s.length();
+        Boolean[] dp = new Boolean[n+1];
+
+        dp[0] = true;
+
+        for (Integer i=1; i<=n; i++){
+
+            for (Integer j=i-1; j>=0; j--){
+
+                if (dp[j] && wordDict.contains(s.substring(j,i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
