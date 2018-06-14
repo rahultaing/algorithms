9e42974c8df9b447a1ee1601e28c7f15ff8a9299
Rahul Taing <rahul.taing@outlook.com>
2018-06-13 23:38:10 -0700

    updating main class

--- src/com/algorithms/Main.java
+++ src/com/algorithms/Main.java
@@ -6,9 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
-        LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
-        longestCommonPrefix.longestCommonPrefix();
+        
         System.out.println("hello world");
     }
 }
