47010099d955ab279da355fb00bc213c25cccb92
Rahul Taing <rahult@zillowgroup.com>
2020-03-29 16:11:34 -0700

    wildcard matching via dp

--- src/com/algorithms/strings/WildcardMatching.java
+++ src/com/algorithms/strings/WildcardMatching.java
@@ -14,6 +14,9 @@ p could be empty and contains only lowercase letters a-z, and characters like ? 
 */
 
 /*TODO: determine time complexity*/
+
+// this is a much better solution using dp https://github.com/mission-peace/interview/blob/master/src/com/interview/dynamic/WildCardMatching.java
+
 public class WildcardMatching {
 
     public boolean isMatch(String s, String p) {
@@ -48,5 +51,50 @@ public class WildcardMatching {
         }
 
         return p1 == p.length();
+    }
+
+    // dp way of doing it
+    public boolean isMatch(String s, String p){
+
+        char[] sa = s.toCharArray();
+        char[] pa = p.toCharArray();
+
+        int writeIndex = 0;
+        boolean isFirst = true;
+        for (int i=0; i<p.length() ; i++){
+
+            if (pa[i] == '*'){
+                if (isFirst){
+                    pa[writeIndex++] = pa[i];
+                    isFirst = false;
+                }
+            }
+            else {
+                pa[writeIndex++] = pa[i];
+                isFirst = true;
+            }
+        }
+
+        boolean[][] dp = new boolean[s.length()+1][writeIndex+1];
+
+        dp[0][0] = true;
+        if (writeIndex > 0 && pa[0] == '*'){
+                dp[0][1] = true;
+        }
+
+        for (int i=1; i<=s.length(); i++){
+
+            for (int j=1; j<=writeIndex; j++){
+
+                if (sa[i] == pa[i] || pa[i] == '?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if (pa[j] == '*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+            }
+        }
+
+        return dp[s.length()][writeIndex];
     }
 }
