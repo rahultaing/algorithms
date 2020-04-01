c12f9af4f79f272e8d5d2c756266d49cf757ba8e
Rahul Taing <rahult@zillowgroup.com>
2020-03-31 19:54:53 -0700

    word break

--- src/com/algorithms/dp/WordBreak.java
+++ src/com/algorithms/dp/WordBreak.java
@@ -5,6 +5,12 @@ import java.util.HashSet;
 /*
 Input: s = "leetcode", wordDict = ["leet", "code"]
 Output: true
+
+https://leetcode.com/problems/word-break/
+
+https://leetcode.com/problems/word-break/discuss/43790/Java-implementation-using-DP-in-two-ways
+
+https://www.youtube.com/watch?v=WepWFGxiwRs&t=831s
 */
 
 public class WordBreak {
