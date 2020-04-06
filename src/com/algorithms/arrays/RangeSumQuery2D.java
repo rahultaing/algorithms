673d00cb7644ffb32db7e6674e500cc5096d4224
Rahul Taing <rahult@zillowgroup.com>
2020-04-06 08:24:10 -0700

    range sum query 2d

---
+++ src/com/algorithms/arrays/RangeSumQuery2D.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/range-sum-query-2d-immutable/
+// https://leetcode.com/problems/range-sum-query-2d-immutable/discuss/75350/Clean-C%2B%2B-Solution-and-Explaination-O(mn)-space-with-O(1)-time
+
+public class NumMatrix {
+
+    private int[][] dp;
+    public NumMatrix(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        dp = new int[n+1][m+1];
+        
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=m; j++){
+
+                dp[i][j] = dp[i][j-1] + dp[i-1][j] + matrix[i-1][j-1] - dp[i-1][j-1];
+            }
+        }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        
+        return dp[row2+1][col2+1] - dp[row1][col2+1] - dp[row2+1][col1] + dp[row1][col1];
+    }
+}
