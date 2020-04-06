18572049b3333e4cd667dadd0052818d053c2802
Rahul Taing <rahult@zillowgroup.com>
2020-04-06 09:32:17 -0700

    longest palindromic subsequence

--- src/com/algorithms/dp/LongestPalindromicSubsequence.java
+++ src/com/algorithms/dp/LongestPalindromicSubsequence.java
@@ -1,3 +1,5 @@
+/*https://github.com/mission-peace/interview/blob/master/src/com/interview/dynamic/LongestPalindromicSubsequence.java */
+
 package com.algorithms.dp;
 
 public class LongestPalindromicSubsequence {
@@ -16,14 +18,42 @@ public class LongestPalindromicSubsequence {
         for (Integer l = 2; l < n; l++){
             for (Integer i = 0; i < n-l+1; i++){
 
-                Integer j = i+l;
+                Integer j = i+l-1;
 
                 if (l==2 && s.charAt(i)==s.charAt(j)){
                     dp[i][j] = 2;
                 }else if (s.charAt(i) == s.charAt(j)){
                     dp[i][j] = dp[i+1][j-1] + 2;
                 }else {
-                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j+1]);
+                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[0][n-1];
+    }
+
+
+    public int longestPalindromicSubsequence(String s){
+
+        int n = s.length();
+
+        int[][] dp = new int[n][n];
+
+        for (int i=0; i<n; i++){
+            dp[i][i] = 1;
+        }
+
+        for (int d=1; d<n; d++){
+            for (int i=0; i<n-d; i++){
+
+                int j= i+d;
+
+                if (s.charAt(i) == s.charAt(j)){
+                    dp[i][j] = 2 + dp[i+1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
                 }
             }
         }
