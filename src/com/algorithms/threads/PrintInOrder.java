98a8dbcdf06b2463cd6506880526da5e0902dccf
Rahul Taing <rahult@zillowgroup.com>
2020-04-08 19:11:57 -0700

    print in order

---
+++ src/com/algorithms/threads/PrintInOrder.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/print-in-order/discuss/332890/Java-Basic-semaphore-solution-8ms-36MB
+
+import java.util.concurrent.Semaphore;
+
+class Foo {
+
+    Semaphore run2 = new Semaphore(0);
+    Semaphore run3 = new Semaphore(0);
+
+    public Foo() {
+        
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        run2.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        run2.acquire();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        run3.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        run3.acquire();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
