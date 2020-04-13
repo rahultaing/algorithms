58c97ce35e5545aa02ac08508c773ce1782fb585
Rahul Taing <rahult@zillowgroup.com>
2020-04-13 09:54:15 -0700

    populate next right pointers

--- src/com/algorithms/trees/PopulateNextRightPointers2.java
+++ src/com/algorithms/trees/PopulateNextRightPointers2.java
@@ -1,34 +1,72 @@
+/*
+https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
+
+this is better / clearer 
+
+https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/discuss/37811/Simple-solution-using-constant-space
+*/
 package com.algorithms.trees;
 
 public class PopulateNextRightPointers2 {
 
-    public void connect(TreeLinkNode root) {
+    public void connect(TreeLinkNode node) {
 
-        if (root == null){
+        if (node == null){
             return;
         }
 
-        while(root != null){
+        TreeLinkNode dummy = new TreeLinkNode(0);
+        TreeLinkNode pre = dummy;
 
-            TreeLinkNode temp = new TreeLinkNode(0);
-            TreeLinkNode curNode = temp;
+        while (node != null){
 
-            while (root != null){
-
-                if (root.left != null){
-                    curNode.next = root.left;
-                    curNode = curNode.next;
-                }
-
-                if (root.right != null){
-                    curNode.next = root.right;
-                    curNode = curNode.next;
-                }
-
-                root = root.next;
+            if (node.left != null){
+                pre.next = node.left;
+                pre = pre.next;
             }
 
-            root = temp.next;
+            if (node.right != null){
+                pre.next = node.right;
+                pre = pre.next;
+            }
+
+            node = node.next;
+
+            if (node == null){
+                pre = dummy;
+                node = dummy.next;
+                dummy.next = null;
+            }
+        }
+    }
+
+    // solution 2
+
+    public void connect2(TreeLinkNode node){
+
+
+        while (node!=null){
+
+            TreeLinkNode dummy = new TreeLinkNode(0);
+            TreeLinkNode cur = dummy;
+
+            while (node!=null){
+
+                if (node.left != null){
+
+                    cur.next= node.left;
+                    cur = cur.next;
+                }
+
+                if (node.right!=null){
+                    cur.next = node.right;
+                    cur = cur.next;
+                }
+
+                node = node.next;
+            }
+
+            node = dummy.next;
         }
     }
 }
