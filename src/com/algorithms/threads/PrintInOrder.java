fd4cf88136fb563c6e695e994888037aa64dcb4d
Rahul Taing <rahult@zillowgroup.com>
2020-04-13 09:17:14 -0700

    added try-finally to print in order

--- src/com/algorithms/threads/PrintInOrder.java
+++ src/com/algorithms/threads/PrintInOrder.java
@@ -14,15 +14,23 @@ class Foo {
     public void first(Runnable printFirst) throws InterruptedException {
         
         // printFirst.run() outputs "first". Do not change or remove this line.
-        printFirst.run();
-        run2.release();
+        try{
+            printFirst.run();
+        }
+        finally{
+            run2.release();
+        }
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
         run2.acquire();
         // printSecond.run() outputs "second". Do not change or remove this line.
-        printSecond.run();
-        run3.release();
+        try{
+            printSecond.run();
+        }
+        finally{
+            run3.release();
+        }
     }
 
     public void third(Runnable printThird) throws InterruptedException {
