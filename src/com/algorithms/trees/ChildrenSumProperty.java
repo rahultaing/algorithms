7691041e940e887e8636a39c6c779bf693389006
Rahul Taing <rahult@zillowgroup.com>
2020-07-05 16:27:48 -0700

    children sum property

--- src/com/algorithms/trees/ChildrenSumProperty.java
+++ src/com/algorithms/trees/ChildrenSumProperty.java
@@ -1,5 +1,10 @@
 // https://www.geeksforgeeks.org/check-for-children-sum-property-in-a-binary-tree/
 
+/*Given a binary tree, write a function that returns true if the tree satisfies below property.
+For every node, data value must be equal to sum of data values in left and right children. Consider data value as 0 for NULL children. Below tree is an example
+
+ */
+
 public class Solution{
 
     public boolean childrenSumProperty(TreeNode node){
