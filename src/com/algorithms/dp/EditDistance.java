b0634227127a7f1834cf0886ab057cf9b0520cc9
Rahul Taing <rahult@zillowgroup.com>
2020-07-06 14:36:23 -0700

    added link to problem

--- src/com/algorithms/dp/EditDistance.java
+++ src/com/algorithms/dp/EditDistance.java
@@ -1,5 +1,24 @@
 package com.algorithms.dp;
-/*https://www.youtube.com/watch?v=We3YDTzNXEk */
+/*https://www.youtube.com/watch?v=We3YDTzNXEk 
+
+https://leetcode.com/problems/edit-distance/
+
+Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+
+You have the following 3 operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+Example 1:
+
+Input: word1 = "horse", word2 = "ros"
+Output: 3
+Explanation: 
+horse -> rorse (replace 'h' with 'r')
+rorse -> rose (remove 'r')
+rose -> ros (remove 'e')
+*/
 public class EditDistance {
 
     public int minDistance(String word1, String word2) {
