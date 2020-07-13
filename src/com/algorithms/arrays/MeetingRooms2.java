92aebbd89ce3965e34b0fe8a2789d12a5801c357
Rahul Taing <rahult@zillowgroup.com>
2020-07-12 19:22:04 -0700

    adding problem statement

--- src/com/algorithms/arrays/MeetingRooms2.java
+++ src/com/algorithms/arrays/MeetingRooms2.java
@@ -1,4 +1,17 @@
 // https://leetcode.com/problems/meeting-rooms-ii/solution/
+/**
+ * 
+ * Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
+
+Example 1:
+
+Input: [[0, 30],[5, 10],[15, 20]]
+Output: 2
+Example 2:
+
+Input: [[7,10],[2,4]]
+Output: 1
+ */
 import java.util.PriorityQueue;
 
 public class Solution{
