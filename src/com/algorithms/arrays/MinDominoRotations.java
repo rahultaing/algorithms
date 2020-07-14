23d48050fccc3fc493092cf1219fa4f92c6f36f8
Rahul Taing <rahult@zillowgroup.com>
2020-07-13 17:23:41 -0700

    added problem statement and algo

--- src/com/algorithms/arrays/MinDominoRotations.java
+++ src/com/algorithms/arrays/MinDominoRotations.java
@@ -1,3 +1,25 @@
+/**
+ * https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/
+ * In a row of dominoes, A[i] and B[i] represent the top and bottom halves of the i-th domino.  (A domino is a tile with two numbers from 1 to 6 - one on each half of the tile.)
+
+We may rotate the i-th domino, so that A[i] and B[i] swap values.
+
+Return the minimum number of rotations so that all the values in A are the same, or all the values in B are the same.
+
+If it cannot be done, return -1.
+
+Algorithm
+
+Pick up the first element. It has two sides: A[0] and B[0].
+
+Check if one could make all elements in A row or B row to be equal to A[0]. If yes, return the minimum number of rotations needed.
+
+Check if one could make all elements in A row or B row to be equal to B[0]. If yes, return the minimum number of rotations needed.
+
+Otherwise return -1.
+
+ */
+
 public class Solution{
 
     private int n;
