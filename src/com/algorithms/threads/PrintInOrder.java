596a78c94b233dfd0bfb56f181ad2fb311b0428d
Rahul Taing <rahult@zillowgroup.com>
2020-07-27 12:19:52 -0700

    added problem statement

--- src/com/algorithms/threads/PrintInOrder.java
+++ src/com/algorithms/threads/PrintInOrder.java
@@ -1,5 +1,24 @@
 // https://leetcode.com/problems/print-in-order/discuss/332890/Java-Basic-semaphore-solution-8ms-36MB
 
+/*
+Suppose we have a class:
+
+public class Foo {
+  public void first() { print("first"); }
+  public void second() { print("second"); }
+  public void third() { print("third"); }
+}
+The same instance of Foo will be passed to three different threads. Thread A will call first(), thread B will call second(), and thread C will call third(). Design a mechanism and modify the program to ensure that second() is executed after first(), and third() is executed after second().
+
+ 
+
+Example 1:
+
+Input: [1,2,3]
+Output: "firstsecondthird"
+Explanation: There are three threads being fired asynchronously. The input [1,2,3] means thread A calls first(), thread B calls second(), and thread C calls third(). "firstsecondthird" is the correct output.
+Example 2:
+ */
 import java.util.concurrent.Semaphore;
 
 class Foo {
