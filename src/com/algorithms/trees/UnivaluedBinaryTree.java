0596281dc67b93648ad60450f89e19db9c3773d3
Rahul Taing <rahul.taing@outlook.com>
2020-09-21 15:49:03 -0700

    univalued tree

---
+++ src/com/algorithms/trees/UnivaluedBinaryTree.java
@@ -0,0 +1,11 @@
+/*https://leetcode.com/problems/univalued-binary-tree/solution/ */
+
+class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+         boolean left_correct = (root.left == null ||
+                (root.val == root.left.val && isUnivalTree(root.left)));
+        boolean right_correct = (root.right == null ||
+                (root.val == root.right.val && isUnivalTree(root.right)));
+        return left_correct && right_correct;
+    }
+}
